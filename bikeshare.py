bbf6da2468ea84f334d404ca4be42236c4a4b24c
Sy Vu Van <vuvansy1992kt@gmail.com>
2024-07-24 09:51:12 +0700

    refactor code: change title of analysis for each segment analysis

--- bikeshare.py
+++ bikeshare.py
@@ -86,7 +86,7 @@ def load_data(city, month, day):
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print('\nCalculating The Most Frequent Times of Travel.\n')
     start_time = time.time()
 
     # TO DO: display the most common month
@@ -106,7 +106,7 @@ def time_stats(df):
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
+    print('\nCalculating The Most Popular Stations and Trip.\n')
     start_time = time.time()
 
     # TO DO: display most commonly used start station
@@ -125,7 +125,7 @@ def station_stats(df):
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
-    print('\nCalculating Trip Duration...\n')
+    print('\nCalculating Trip Duration.\n')
     start_time = time.time()
 
     # TO DO: display total travel time
@@ -143,7 +143,7 @@ def trip_duration_stats(df):
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
+    print('\nCalculating User Stats.\n')
     start_time = time.time()
 
     # TO DO: Display counts of user types
